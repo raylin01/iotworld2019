6226f297c741d2395e6c681776d76cdc9e7e42dc
Anish Muthali <anishmuthali@gmail.com>
2019-05-13 15:40:09 -0700

    Updated file name

--- ML/svm.py
+++ ML/svm.py
@@ -3,14 +3,15 @@ from sklearn import preprocessing
 import numpy as np
 import pandas as pd
 
-dataframe = pd.read_csv('squaw_valley_delta.csv', sep=',', header=0)
+dataframe = pd.read_csv('squaw_valley_daily.csv', sep=',', header=0)
 dataframe = dataframe.dropna()
 print(dataframe.isnull().any().any())
 del dataframe['Date']
 npdata = np.asarray(dataframe)
 x = npdata[:,:4]
 y = npdata[:, 4]
-x = preprocessing.normalize(x, axis=0)
+x = preprocessing.normalize(x, axis=1)
+print(x)
 
 classifier = svm.SVR()
 classifier.fit(x,y)
