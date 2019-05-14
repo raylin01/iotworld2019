2eedd841958541bbb234a39f892b7a3eff5e20d2
anishmuthali <anishmuthali@gmail.com>
2019-05-13 23:45:58 -0700

    Prelim test script for lstm

---
+++ ML/lstm_test.py
@@ -0,0 +1,21 @@
+from keras.models import load_model
+
+model = load_model('lstm.h5')
+
+def pred(vals):
+    vals = np.array(vals)
+    vals = vals.reshape((1, vals.shape[0], vals.shape[1]))
+    return model.predict(vals)
+
+data = [
+    [25, 0],
+    [12, 0.2],
+    [11, 0.2],
+    [18, 0.6],
+    [18, 0],
+    [3, 0.1],
+    [25, 0]
+    ]
+
+result = pred(data)
+print(result)
