de6d0cdc08405e2bfe5d790feeb4c246b88a20c3
Anish Muthali <anishmuthali@gmail.com>
2019-05-13 19:19:04 -0700

    Updated classifier type

--- ML/svm.py
+++ ML/svm.py
@@ -1,27 +1,35 @@
 from sklearn import svm
+from sklearn import tree
 from sklearn import preprocessing
+from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import *
+from sklearn.tree import *
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import pandas as pd
 
-dataframe = pd.read_csv('squaw_valley_2000.csv', sep=',', header=0)
+dataframe = pd.read_csv('atlanta_1980.csv')
 dataframe = dataframe.dropna()
 print(dataframe.isnull().any().any())
 del dataframe['Date']
+dataframe["Change In Soil Moisture Percent -8in (pct)"] = np.where(dataframe["Change In Soil Moisture Percent -8in (pct)"] >= 0, 0, dataframe["Change In Soil Moisture Percent -8in (pct)"])
+dataframe["Change In Soil Moisture Percent -8in (pct)"] = np.where(dataframe["Change In Soil Moisture Percent -8in (pct)"] < 0, 1, dataframe["Change In Soil Moisture Percent -8in (pct)"])
 npdata = np.asarray(dataframe)
-npdata = preprocessing.normalize(npdata, axis=1)
+np.random.shuffle(npdata)
+print(npdata.shape)
 x = npdata[:,:4]
-y = npdata[:, 4]
-#x = preprocessing.normalize(x, axis=0)
-#y = preprocessing.normalize(y.reshape(-1, 1), axis=0)
+y = npdata[:, 4].reshape(-1,1)
+y = y.reshape((y.shape[0]))
 print(x)
 print(y)
-x_train = x[:2910,:]
-y_train = y[:2910]
-x_test = x[2910:3234,:]
-y_test = y[2910:3234]
+train_split = int(0.9*x.shape[0])
+x_train = x[:train_split,:]
+y_train = y[:train_split]
+x_test = x[train_split:x.shape[0],:]
+y_test = y[train_split:x.shape[0]]
 
-classifier = BayesianRidge()
+classifier = DecisionTreeClassifier(criterion='entropy', max_features=4)
 classifier.fit(x_train,y_train)
 
 score = classifier.score(x_train,y_train)
